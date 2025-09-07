364bff49a71d19856f72e907c3545635f712e4ec
ACurtolo <curtoloa@gmail.com>
2025-09-07 10:39:31 +1000

    Fix Boundary_distance Issue in API

--- backend/ExemptAssessAPI.py
+++ backend/ExemptAssessAPI.py
@@ -125,7 +125,12 @@ in a heritage item or a draft heritage item, on land in a foreshore area or in a
     (2) There must not be more than 2 developments per lot.
 
 """
-
+def parse_float(value, default=0.0):
+    try:
+        return float(value)
+    except (TypeError, ValueError):
+        return default
+    
 def patio_check():
     """ This function provides the rule-set for Exempt Development of a Patio under the SEPP
         returning a string indicating if the proposed development is Exempt or if not, returning
@@ -171,7 +176,7 @@ def patio_check():
     if attributes["boundary_distance"] < 900:
         return "❌ Not Exempt: Too close to boundary. (SEPP, Part 2, Division 1, Subdivision 6, 2.12 (1)(f)(ii))"
     elif attributes["boundary_distance"] < 5000:
-        if attributes["zoning"] not in ["R5", "RU1", "RU2", "RU3", "RU4", "RU6"]:
+        if attributes["zoning"] in ["R5", "RU1", "RU2", "RU3", "RU4", "RU6"]:
             return "❌ Not Exempt: Too close to boundary. (SEPP, Part 2, Division 1, Subdivision 6, 2.12 (1)(f)(i))" 
 
     if attributes["metal"] == "yes":
@@ -238,7 +243,7 @@ def shed_check():
     if attributes["boundary_distance"] < 900:
         return "❌ Not Exempt: Too close to boundary. (SEPP, Part 2, Division 1, Subdivision 6, 2.18 (1)(d)(ii))"
     elif attributes["boundary_distance"] < 5000:
-        if attributes["zoning"] not in ["R5", "RU1", "RU2", "RU3", "RU4", "RU6"]:
+        if attributes["zoning"] in ["R5", "RU1", "RU2", "RU3", "RU4", "RU6"]:
             return "❌ Not Exempt: Too close to boundary. (SEPP, Part 2, Division 1, Subdivision 6, 2.18 (1)(d)(i))" 
 
     if attributes["building_line"] == "no":
@@ -280,10 +285,11 @@ def shed_check():
 
 
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 # Create API
 app = Flask(__name__)
-
+CORS(app)  # This enables CORS for all routes
 
 @app.route("/get-assessment-result/", methods=["POST","GET"])
 def API_assessment():
